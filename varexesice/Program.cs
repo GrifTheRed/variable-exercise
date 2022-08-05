298bb2ec59184ced096e6b66ba91317116246732
Derek Griffin <griffinderek6@gmail.com>
2022-08-05 12:23:48 -0600

    variable exercise

---
+++ varexesice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace varexesice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables deceralations
+            string dogName;
+
+            int dogAge;
+
+            double cost;
+
+            decimal thisMuch;
+
+            char nickName;
+
+            bool isBestBoy;
+
+            // variables initializtion
+
+            dogName = "Charlie";
+
+            dogAge = 4;
+
+            isBestBoy = true;
+
+            cost = 400.00;
+
+            thisMuch = 70;
+
+            nickName = 'C';
+
+
+
+            Console.WriteLine($"{dogName} is {dogAge} and is the best boy {isBestBoy}");
+            Console.WriteLine($"Sometimes I call him {nickName}. I paid {cost} for him but would of paid anything");
+            Console.WriteLine($"Hes a big ol' boy weighing in at {thisMuch}");
+        }
+    }
+}
+
